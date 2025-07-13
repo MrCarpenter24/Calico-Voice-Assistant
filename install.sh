d10ff229ac1882fc83f3cd28e8bd12bc11248be4
Johnathan Phillips <mrcarpenter240@gmail.com>
2025-07-13 13:24:33 -0400

    Update install.sh
    Calico (^oxo^)~

--- install.sh
+++ install.sh
@@ -156,7 +156,7 @@ cat > "$DESKTOP_FILE_PATH" << EOL
 [Desktop Entry]
 Version=1.0
 Type=Application
-Name=Calico Voice Assistant
+Name=Calico (^oxo^)~
 Comment=A launcher for the Calico Voice Assistant
 Exec="$PYTHON_VENV_DIR/bin/python3" "$APP_DIR/launcher.py"
 Icon=$ICON_PATH
